a3df6a21e8ad2fb316fdceeef280c6b2eedcf259
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-12-08 10:56:12 +1100

    NavBar scaffold

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const NavContainer = styled.nav`
+  width: 100vw;
+  height: 3rem;
+  background-color: red;
+`;
+
+export const NavBar = () => {
+  return <NavContainer />;
+};
