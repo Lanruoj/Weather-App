6d1f1487f56d90fe4e6659d4c02d626287e5231d
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-12-08 11:32:17 +1100

    Add basic styling to NavBar elements

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,36 @@
 import styled from "styled-components";
 
 const NavContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100vw;
   height: 4rem;
   background-color: #def7ff;
 `;
 
 const MenuButton = styled.button`
+  margin-left: 0.5rem;
   width: 4rem;
   height: 4rem;
   border: none;
   font-size: 3rem;
   background-color: #def7ff;
-  color: #4b4b4b;
+  color: #1d1d91;
+`;
+
+const NavTitle = styled.h1`
+  margin: 0 1rem 0 0;
+  font-size: 2rem;
+  font-family: "Roboto Mono", monospace;
+  color: #1d1d91;
 `;
 
 export const NavBar = () => {
   return (
     <NavContainer>
       <MenuButton>&#62;</MenuButton>
+      <NavTitle>weathercycle</NavTitle>
     </NavContainer>
   );
 };
