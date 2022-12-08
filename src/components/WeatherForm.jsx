2a2e5fcdf7c27104dc8b7e4ebbd291f5f128ad60
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-12-08 14:28:31 +1100

    Add <SubmitButton>, handleChange(), textValue state

--- src/components/WeatherForm.jsx
+++ src/components/WeatherForm.jsx
@@ -1,26 +1,42 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const Form = styled.form`
   background-color: red;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
 `;
 
 const SearchField = styled.input.attrs({ type: "search" })`
-  /* background-color: blue; */
   height: 2rem;
   width: 10rem;
 `;
 
+const SubmitButton = styled.button`
+  color: blue;
+  height: 2rem;
+  width: 4rem;
+`;
+
 export const WeatherForm = () => {
+  const [textValue, setTextValue] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("onSubmit");
   };
 
+  const handleChange = (e) => {
+    setTextValue(e.target.value);
+    // console.log(textValue);
+  };
+
   return (
     <Form onSubmit={handleSubmit}>
-      <SearchField />
+      <SearchField onChange={handleChange} />
+      <SubmitButton>Go</SubmitButton>
     </Form>
   );
 };
