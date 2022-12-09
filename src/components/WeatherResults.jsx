a27a66f5eef67f445581a78ae47c3fe1c3ab0770
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-12-09 17:04:24 +1100

    Create groupResultsByDay()

--- src/components/WeatherResults.jsx
+++ src/components/WeatherResults.jsx
@@ -3,15 +3,42 @@ import { useSearchContext } from "../utils/SearchContext";
 
 export const WeatherResults = () => {
   const { results } = useSearchContext();
+  // const resultDays =
 
   const getDayNumber = (datetime) => {
     const date = new Date(datetime).getDay();
     return date;
   };
 
+  const groupResultsByDay = (results) => {
+    // let resultsObj = {};
+    // resultsObj["monday"] = results.filter(
+    //   (result) => getDayNumber(result["dt_txt"]) === index
+    // );
+    // let sortedResults = results.reduce((prev, current) {
+    //   prev[current.dt] = prev[current.dt] || [];
+    //   prev[current.dt].push(current);
+    //   return prev
+    // })
+    // let sortedResults = results.groupBy((result) =>
+    //   getDayNumber(result["dt_text"])
+    // );
+    const sortedByMs = results.reduce((prev, current) => {
+      if (!prev[getDayNumber(current["dt_txt"])]) {
+        prev[getDayNumber(current["dt_txt"])] = [];
+      }
+      prev[getDayNumber(current["dt_txt"])].push(current);
+      return prev;
+    }, {});
+    // console.log(sortedByMs);
+    return sortedByMs;
+  };
+
   useEffect(() => {
     if (results) {
-      console.log(results);
+      // console.log(results);
+      const resultsGroupedByDays = groupResultsByDay(results);
+      console.log(resultsGroupedByDays);
       // const datetime = results[0]["dt_txt"];
       // const date = getDayNumber(results[0]["dt_txt"]);
       // console.log(date);
